d4046b200831a096b7ad1e7b19162f5f1ef1cf24
Vlad <wlad3430@gmail.com>
2021-10-08 13:21:00 +0300

    Размер кисти

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const canvas = document.getElementById('jsCanvas')
 const ctx = canvas.getContext('2d')
+const colors = document.getElementsByClassName('jsColor')
+const range = document.getElementById('jsRange')
+const mode = document.getElementById('jsMode')
 
 canvas.height = 500
 canvas.width = 900
@@ -7,6 +10,7 @@ canvas.width = 900
 ctx.lineWidth = 2.5
 ctx.strokeStyle = '#2c2c2c'
 let painting = false
+let filling = false
 
 function startPainting() {
   painting = true
@@ -39,4 +43,36 @@ if (canvas) {
   canvas.addEventListener('mousedown', onMouseDown)
   canvas.addEventListener('mouseup', stopPaining)
   canvas.addEventListener('mouseleave', stopPaining)
+}
+
+function handleColorClick(e) {
+  const color = e.target.style.backgroundColor
+  ctx.strokeStyle = color
+  console.log(color);
+}
+
+function handleRangeChange(e) {
+  const range = e.target.value
+  ctx.lineWidth = range
+}
+
+function handleModeClick() {
+  if (filling === true) {
+    filling = false
+    mode.innerText = 'Заливка'
+  } else {
+    filling = true
+    mode.innerText = 'Рисование'
+  }
+}
+
+
+Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick))
+
+if (range) {
+  range.addEventListener('input', handleRangeChange)
+}
+
+if (mode) {
+  mode.addEventListener('click', handleModeClick)
 }
