553f3bd0a5213f478a6b8b7f6fc17a8c425d15ef
Vlad <wlad3430@gmail.com>
2021-10-08 14:27:30 +0300

    Завершение проекта

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const ctx = canvas.getContext('2d')
 const colors = document.getElementsByClassName('jsColor')
 const range = document.getElementById('jsRange')
 const mode = document.getElementById('jsMode')
+const save = document.getElementById('jsSave')
 
-canvas.height = 500
-canvas.width = 900
+const INITIAL_COLOR = '#2c2c2c'
+const CANVAS_SIZE = 700
+
+canvas.height = CANVAS_SIZE
+canvas.width = CANVAS_SIZE
+
+ctx.fillStyle = 'white'
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE)
 
 ctx.lineWidth = 2.5
-ctx.strokeStyle = '#2c2c2c'
+ctx.strokeStyle = INITIAL_COLOR
+ctx.fillStyle = INITIAL_COLOR
+
 let painting = false
 let filling = false
 
@@ -37,18 +46,29 @@ function onMouseDown(e) {
   painting = true
 }
 
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE)
+  }
+}
+
+function handleCM(e) {
+  e.preventDefault()
+}
 
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove)
   canvas.addEventListener('mousedown', onMouseDown)
   canvas.addEventListener('mouseup', stopPaining)
   canvas.addEventListener('mouseleave', stopPaining)
+  canvas.addEventListener('click', handleCanvasClick)
+  canvas.addEventListener('contextmenu', handleCM)
 }
 
 function handleColorClick(e) {
   const color = e.target.style.backgroundColor
   ctx.strokeStyle = color
-  console.log(color);
+  ctx.fillStyle = color
 }
 
 function handleRangeChange(e) {
@@ -66,13 +86,19 @@ function handleModeClick() {
   }
 }
 
+function handleSaveClick() {
+  const image = canvas.toDataURL()
+  const link = document.createElement('a')
+  link.href = image
+  link.download = 'Рисунок'
+  link.click()
+  
+}
 
 Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick))
 
-if (range) {
-  range.addEventListener('input', handleRangeChange)
-}
+if (range) range.addEventListener('input', handleRangeChange)
 
-if (mode) {
-  mode.addEventListener('click', handleModeClick)
-}
+if (mode) mode.addEventListener('click', handleModeClick)
+
+if (save) save.addEventListener('click', handleSaveClick)
